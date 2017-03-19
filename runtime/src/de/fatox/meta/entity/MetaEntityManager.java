652f58d878d1f745e3870effe49a0c8d326b401c
Frotty <frotty@sunayama.de>
2017-03-19 21:39:55 +0100

    cleanup entity manager

--- runtime/src/de/fatox/meta/entity/MetaEntityManager.java
+++ runtime/src/de/fatox/meta/entity/MetaEntityManager.java
@@ -1,8 +1,6 @@
 package de.fatox.meta.entity;
 
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Pixmap;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.VertexAttributes;
 import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Model;
@@ -23,19 +21,14 @@ public class MetaEntityManager implements EntityManager<Meta3DEntity> {
 
     public MetaEntityManager() {
         Meta.inject(this);
-        Pixmap pxmp = new Pixmap(1, 1, Pixmap.Format.RGB888);
-        pxmp.setColor(Color.LIGHT_GRAY);
-        pxmp.drawPixel(0, 0);
-        Texture whiteTex = new Texture(pxmp);
         ModelBuilder modelBuilder = new ModelBuilder();
-        Model model = assetProvider.get("models/cryofan.g3db", Model.class);
         final Material material = new Material(ColorAttribute.createDiffuse(Color.LIGHT_GRAY));
-        final Material material2 = new Material();
         final long attributes = VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal | VertexAttributes.Usage.ColorUnpacked;
-        Model box2 = modelBuilder.createXYZCoordinates(32, material2, VertexAttributes.Usage.Position| VertexAttributes.Usage.ColorUnpacked);
-        Model box = modelBuilder.createLineGrid(64,64, 32, 32, material, attributes);
-        addEntity(new Meta3DEntity(new Vector3(0, 0, 0), box));
-        addEntity(new Meta3DEntity(new Vector3(0, 0, 0), box2));
+
+        Model xyzCoordinates = modelBuilder.createXYZCoordinates(24, new Material(), attributes);
+        Model grid = modelBuilder.createLineGrid(64,64, 32, 32, material, attributes);
+        addEntity(new Meta3DEntity(new Vector3(0, 0, 0), grid));
+        addEntity(new Meta3DEntity(new Vector3(0, 0, 0), xyzCoordinates));
     }
 
     @Override
