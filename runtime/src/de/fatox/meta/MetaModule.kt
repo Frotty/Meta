6af8ed1bc8389e743442817c02052b8f589a5411
Frotty <Frotty@users.noreply.github.com>
2020-05-13 13:24:55 +0200

    Update MetaModule.kt

--- runtime/src/de/fatox/meta/MetaModule.kt
+++ runtime/src/de/fatox/meta/MetaModule.kt
@@ -1,5 +1,6 @@
 package de.fatox.meta
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.PerspectiveCamera
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
