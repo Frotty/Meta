63b6324213a796b5f81e59e8183b64e7c5ad02c1
PinkieSwirl <PinkieSwirl@users.noreply.github.com>
2020-08-13 22:17:20 +0200

    Made object and merged companion object

--- runtime/src/de/fatox/meta/util/GoldenRatio.kt
+++ runtime/src/de/fatox/meta/util/GoldenRatio.kt
@@ -1,11 +1,7 @@
 package de.fatox.meta.util
 
-enum class GoldenRatio {
-	;
-
-	companion object {
-		const val A = 0.618f
-		const val B = 0.382f
-		const val C = 0.236f
-	}
+object GoldenRatio {
+	const val A = 0.618f
+	const val B = 0.382f
+	const val C = 0.236f
 }
