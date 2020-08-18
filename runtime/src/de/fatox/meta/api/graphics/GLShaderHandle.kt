3119ee90c7a60a9034ecd824dc9bd1e39687eef8
PinkieSwirl <PinkieSwirl@users.noreply.github.com>
2020-08-19 01:40:55 +0200

    Fixed GLShaderHandle kotlin implementation

--- runtime/src/de/fatox/meta/api/graphics/GLShaderHandle.kt
+++ runtime/src/de/fatox/meta/api/graphics/GLShaderHandle.kt
@@ -6,60 +6,55 @@ import de.fatox.meta.api.model.GLShaderData
 import de.fatox.meta.api.model.RenderTargetData
 import java.io.BufferedReader
 import java.io.IOException
+import java.util.regex.Matcher
 import java.util.regex.Pattern
 
 /**
  * Created by Frotty on 02.07.2016.
  */
-class GLShaderHandle(val shaderHandle: FileHandle, vertexHandle: FileHandle?, fragmentHandle: FileHandle?, var data: GLShaderData) {
-    var fragmentHandle = fragmentHandle
-        set(value) {
-            data.fragmentFilePath = fragmentHandle?.path() ?: ""
-        }
-    var vertexHandle = vertexHandle
-        set(value) {
-            data.vertexFilePath = vertexHandle?.path() ?: ""
-        }
+class GLShaderHandle(
+	val shaderHandle: FileHandle,
+	vertexHandle: FileHandle,
+	fragmentHandle: FileHandle,
+	var data: GLShaderData,
+) {
+	var fragmentHandle = fragmentHandle
+		set(value) {
+			field = value.also { data.fragmentFilePath = it.path() }
+		}
+	var vertexHandle = vertexHandle
+		set(value) {
+			field = value.also { data.vertexFilePath = it.path() }
+		}
 
-    var targets = Array<RenderTargetData>()
+	val targets: Array<RenderTargetData> = Array<RenderTargetData>()
 
-    init {
-        fetchRendertargets()
-    }
+	override fun toString(): String = data.name
 
-    private fun fetchRendertargets() {
-        targets.clear()
-        try {
-            BufferedReader(fragmentHandle!!.reader()).use { br ->
-                var line: String? = br.readLine()
-                while (line != null) {
-                    if (line.startsWith("layout")) {
-                        val matcher = outPattern.matcher(line)
-                        if (matcher.matches()) {
-                            val type = matcher.group(4)
-                            val name = matcher.group(6)
-                            targets.add(RenderTargetData(type, name))
-                        }
-                    }
-                    line = br.readLine()
-                }
-            }
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-
-        if (targets.size == 0) {
-            // add default gl out
-            targets.add(RenderTargetData("vec4", "gl_FragColor"))
-        }
-    }
-
-    override fun toString(): String {
-        return data.name
-    }
-
-    companion object {
-
-        private val outPattern = Pattern.compile("(.*)(out)(\\s)+(vec[2-4])(\\s)+(\\w+)(;)")
-    }
+	init {
+		targets.clear()
+		try {
+			BufferedReader(this.fragmentHandle.reader()).use { br ->
+				var line: String
+				while (br.readLine().also { line = it } != null) {
+					if (line.startsWith("layout")) {
+						val matcher: Matcher = outPattern.matcher(line)
+						if (matcher.matches()) {
+							val type = matcher.group(4)
+							val name = matcher.group(6)
+							targets.add(RenderTargetData(type, name))
+						}
+					}
+				}
+			}
+		} catch (e: IOException) {
+			e.printStackTrace()
+		}
+		if (targets.size == 0) {
+			// add default gl out
+			targets.add(RenderTargetData("vec4", "gl_FragColor"))
+		}
+	}
 }
+
+private val outPattern: Pattern = Pattern.compile("(.*)(out)(\\s)+(vec[2-4])(\\s)+(\\w+)(;)")
