4fd0161d5002f93ad621a516799307f55f72c202
PinkieSwirl <PinkieSwirl@users.noreply.github.com>
2020-08-21 21:02:36 +0200

    Test mockable animation

--- runtime/src/de/fatox/meta/graphics/animation/MockAnimation.kt
+++ runtime/src/de/fatox/meta/graphics/animation/MockAnimation.kt
@@ -1,19 +1,17 @@
 package de.fatox.meta.graphics.animation
 
+import com.badlogic.gdx.graphics.Pixmap
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.Animation
+import com.badlogic.gdx.graphics.g2d.Gdx2DPixmap
 import com.badlogic.gdx.graphics.g2d.TextureRegion
 import com.badlogic.gdx.graphics.glutils.FloatTextureData
 import com.badlogic.gdx.utils.Array
+import java.nio.ByteBuffer
 
 object MockAnimation : Animation<TextureRegion>(
 	Float.MAX_VALUE,
-	Array(arrayOf(TextureRegion(Texture(FloatTextureData(
-		0,
-		0,
-		0,
-		0,
-		0,
-		false
-	)))))
-)
+	Array(arrayOf(TextureRegion(Texture(
+		Pixmap(Gdx2DPixmap(ByteBuffer.allocateDirect(0), longArrayOf(0,0,0,0)))
+	))
+)))
