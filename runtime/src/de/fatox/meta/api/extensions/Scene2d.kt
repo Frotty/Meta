6f26389fcf26be944801e2021bc79348d6dbe33d
PinkieSwirl <PinkieSwirl@users.noreply.github.com>
2020-08-24 22:04:40 +0200

    Made Listeners more customizable

--- runtime/src/de/fatox/meta/api/extensions/Scene2d.kt
+++ runtime/src/de/fatox/meta/api/extensions/Scene2d.kt
@@ -1,12 +1,17 @@
 package de.fatox.meta.api.extensions
 
+import com.badlogic.gdx.Input
 import com.badlogic.gdx.scenes.scene2d.Actor
 import com.badlogic.gdx.scenes.scene2d.InputEvent
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener.ChangeEvent
 import de.fatox.meta.ui.components.MetaClickListener
 
-inline fun <reified T : Actor> T.onClick(crossinline action: MetaClickListener.(event: InputEvent) -> Unit): T {
-	addListener(object : MetaClickListener() {
+inline fun <reified T : Actor> T.onClick(
+	button: Int = Input.Buttons.LEFT,
+	crossinline action: MetaClickListener.(event: InputEvent) -> Unit,
+): T {
+	addListener(object : MetaClickListener(button) {
 		override fun clicked(event: InputEvent, x: Float, y: Float) {
 			action(event)
 		}
@@ -14,10 +19,10 @@ inline fun <reified T : Actor> T.onClick(crossinline action: MetaClickListener.(
 	return this
 }
 
-inline fun <reified T : Actor> T.onChange(crossinline action: ChangeListener.() -> Unit): T {
+inline fun <reified T : Actor> T.onChange(crossinline action: ChangeListener.(event: ChangeEvent) -> Unit): T {
 	addListener(object : ChangeListener() {
 		override fun changed(event: ChangeEvent, actor: Actor) {
-			action()
+			action(event)
 		}
 	})
 	return this
