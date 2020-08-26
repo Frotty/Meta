36faea3a803ae3b828f2ea6f352d838c9b531d90
PinkieSwirl <PinkieSwirl@users.noreply.github.com>
2020-08-26 19:18:58 +0200

    Meta cleanup

--- runtime/src/de/fatox/meta/Meta.kt
+++ runtime/src/de/fatox/meta/Meta.kt
@@ -8,6 +8,7 @@ import de.fatox.meta.api.DummyPosModifier
 import de.fatox.meta.api.PosModifier
 import de.fatox.meta.api.ui.UIManager
 import de.fatox.meta.injection.MetaInject.Companion.lazyInject
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.io.PrintWriter
 import java.io.StringWriter
@@ -18,25 +19,14 @@ import javax.swing.UnsupportedLookAndFeelException
 import javax.swing.UIManager as JavaUIManager
 
 
-open class Meta : Game {
-	private var lastChange: Long = 0
-	private var lastScreen: Screen? = null
-
+abstract class Meta(protected var modifier: PosModifier, vararg modules: Any) : Game() {
 	protected val firstScreen: Screen by lazyInject()
 	protected val uiManager: UIManager by lazyInject()
 
-	protected var modifier: PosModifier
+	private var lastChange: Long = 0
+	private lateinit var lastScreen: Screen
 
-	@JvmOverloads
-	constructor(modifier: PosModifier = DummyPosModifier) {
-		this.modifier = modifier
-		setUncaughtHandler()
-		metaInstance = this
-		addModule(MetaModule)
-	}
-
-	constructor(modifier: PosModifier, vararg modules: Any) {
-		this.modifier = modifier
+	init {
 		setUncaughtHandler()
 		metaInstance = this
 		for (module in modules) {
@@ -72,19 +62,21 @@ open class Meta : Game {
 		}
 	}
 
+	abstract fun config()
+
 	override fun create() {
 		uiManager.posModifier = modifier
+		config()
 		changeScreen(firstScreen)
 	}
 
 	val lastScreenType: Class<*>
-		get() = lastScreen!!.javaClass
+		get() = lastScreen.javaClass
 
 	companion object {
-		private val log = LoggerFactory.getLogger(Meta::class.java)
-		private var metaInstance: Meta? = null
-		val instance: Meta?
-			get() = if (metaInstance != null) metaInstance else Meta()
+		private val log: Logger = LoggerFactory.getLogger(Meta::class.java)
+		private lateinit var metaInstance: Meta
+		val instance: Meta by lazy { metaInstance }
 
 		fun addModule(module: Any) {
 			module
@@ -92,27 +84,25 @@ open class Meta : Game {
 
 		fun registerMetaAnnotation(annotationClass: Class<*>?) {}
 		fun canChangeScreen(): Boolean {
-			return TimeUtils.millis() > metaInstance!!.lastChange + 150
+			return TimeUtils.millis() > metaInstance.lastChange + 150
 		}
 
 		fun newLastScreen() {
-			if (instance!!.lastScreen != null) {
-				try {
-					changeScreen(instance!!.lastScreen!!.javaClass.newInstance())
-				} catch (e: Exception) {
-					e.printStackTrace()
-				}
+			try {
+				changeScreen(instance.lastScreen.javaClass.newInstance())
+			} catch (e: Exception) {
+				log.error("Failed to create last screen!", e)
 			}
 		}
 
-		fun changeScreen(newScreen: Screen?) {
+		fun changeScreen(newScreen: Screen) {
 			if (canChangeScreen()) {
-				metaInstance!!.lastChange = TimeUtils.millis()
-				val oldScreen = instance!!.getScreen()
+				metaInstance.lastChange = TimeUtils.millis()
+				val oldScreen = instance.getScreen()
 				if (oldScreen != null && !oldScreen.javaClass.isInstance(newScreen)) {
-					instance!!.lastScreen = oldScreen
+					instance.lastScreen = oldScreen
 				}
-				Gdx.app.postRunnable { instance!!.setScreen(newScreen) }
+				Gdx.app.postRunnable { instance.setScreen(newScreen) }
 			}
 		}
 	}
