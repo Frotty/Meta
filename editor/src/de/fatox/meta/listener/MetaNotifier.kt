0a5331ee9f1bf641fbe86dd9b9a7abcc3b376155
PinkieSwirl <PinkieSwirl@users.noreply.github.com>
2020-12-22 15:50:49 +0100

    Test MetaNotifier removeListener

--- editor/src/de/fatox/meta/listener/MetaNotifier.kt
+++ editor/src/de/fatox/meta/listener/MetaNotifier.kt
@@ -1,13 +1,12 @@
 package de.fatox.meta.listener
 
+import com.badlogic.gdx.utils.ObjectSet
+
 abstract class MetaNotifier {
-	private val clickListeners: HashSet<() -> Unit> = HashSet()
+	private val clickListeners: ObjectSet<() -> Unit> = ObjectSet()
 
-	fun addListener(clickListener: () -> Unit) {
-		clickListeners.add(clickListener)
-	}
+	fun addListener(clickListener: () -> Unit): () -> Unit = clickListener.also { clickListeners.add(it) }
+	fun removeListener(clickListener: () -> Unit): Boolean = clickListeners.remove(clickListener)
 
-	fun notifyListeners() {
-		clickListeners.forEach({ listener -> listener.invoke() })
-	}
+	fun notifyListeners(): Unit = clickListeners.forEach { it() }
 }
