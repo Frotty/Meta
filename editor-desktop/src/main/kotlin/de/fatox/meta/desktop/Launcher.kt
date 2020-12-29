d33cc4ee3aaac23d01269dabd1703e412915962e
PinkieSwirl <PinkieSwirl@users.noreply.github.com>
2020-12-29 16:07:08 +0100

    Move preserving history

--- editor-desktop/src/main/kotlin/de/fatox/meta/desktop/Launcher.kt
+++ editor-desktop/src/main/kotlin/de/fatox/meta/desktop/Launcher.kt
@@ -1,20 +1,14 @@
 package de.fatox.meta.desktop
 
-import de.fatox.meta.desktop.MetaDesktopLauncher.init
-import kotlin.jvm.JvmStatic
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
 import de.fatox.meta.EditorMeta
-import de.fatox.meta.desktop.WindowPosModifier
-import de.fatox.meta.desktop.MetaDesktopLauncher
+import de.fatox.meta.desktop.MetaDesktopLauncher.init
 
-object Launcher {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val config = Lwjgl3ApplicationConfiguration()
-        config.setResizable(true)
-        config.setTitle("Meta")
-        config.setWindowIcon("meta-icon.png")
-        val editorMeta = EditorMeta(WindowPosModifier())
-        init(config, editorMeta)
-    }
+fun main() {
+	val config = Lwjgl3ApplicationConfiguration()
+	config.setResizable(true)
+	config.setTitle("Meta")
+	config.setWindowIcon("meta-icon.png")
+	val editorMeta = EditorMeta(WindowPosModifier())
+	init(config, editorMeta)
 }
