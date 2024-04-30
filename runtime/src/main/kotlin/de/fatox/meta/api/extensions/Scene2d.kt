71fc21735c3fae67c45b2d397c800725ae258d2a
Frotty <Frotty@users.noreply.github.com>
2024-05-01 01:11:20 +0200

    disable cursor pointer for disabled actors

--- runtime/src/main/kotlin/de/fatox/meta/api/extensions/Scene2d.kt
+++ runtime/src/main/kotlin/de/fatox/meta/api/extensions/Scene2d.kt
@@ -11,6 +11,7 @@ import com.badlogic.gdx.scenes.scene2d.InputListener
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener.ChangeEvent
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
+import com.badlogic.gdx.scenes.scene2d.utils.Disableable
 import com.badlogic.gdx.utils.Align
 import com.kotcrab.vis.ui.widget.Tooltip
 
@@ -46,11 +47,13 @@ inline fun <reified T : Actor> T.tooltip(text: String, align: Int = Align.center
 inline fun <reified T : Actor> T.cursorPointer() {
 	addListener(object : InputListener() {
 		override fun enter(event: InputEvent?, x: Float, y: Float, pointer: Int, fromActor: Actor?) {
+			if (T::class == Disableable::class && (this@cursorPointer as Disableable).isDisabled) return
 			Gdx.graphics.setSystemCursor(Cursor.SystemCursor.Hand);
 			super.enter(event, x, y, pointer, fromActor)
 		}
 
 		override fun exit(event: InputEvent?, x: Float, y: Float, pointer: Int, toActor: Actor?) {
+			if (T::class == Disableable::class && (this@cursorPointer as Disableable).isDisabled) return
 			Gdx.graphics.setSystemCursor(Cursor.SystemCursor.Arrow);
 			super.exit(event, x, y, pointer, toActor)
 		}
